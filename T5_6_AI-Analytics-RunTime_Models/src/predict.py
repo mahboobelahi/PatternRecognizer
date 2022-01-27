38caa73eb0b1bc65ce1d98f814c5d02ecd4b3b56
Mahboob Elahi <mahboobelahi93@gmail.com>
2022-01-27 10:54:55 +0200

    Contains ML/DL models for deployment to ZDMP-AI-Analytics-RunTime

---
+++ T5_6_AI-Analytics-RunTime_Models/src/predict.py
@@ -0,0 +1,40 @@
+# Importing modules
+import tensorflow  as tf
+import json
+import numpy as np
+
+
+# loading transformer/scaler Object to scale both features between 0 and 1
+# Load_scaler = joblib.load('pallet-scaler.save')
+# Power_scaler = joblib.load('Power-scaler.save')
+
+
+def predict(data_in):
+    """
+
+    :param data: {
+  "powerConsumption": num,
+  "load": num
+}
+    :return:
+    """
+    # loading pre-trained model
+    #data_in=json.loads(data_in)
+    print(type(data_in))
+    print(data_in)
+    "model loading and data transfor"
+    model = tf.keras.models.load_model('./model/M_iter3_1.h5', compile=True)
+    features_1 = np.array(np.append([data_in.get("powerConsumption")], [data_in.get("load")]), ndmin=2)
+    pred = np.argmax(model.predict(features_1), axis=1) # need to pus that vakue to message bus
+    json_results = json.dumps({"BT_Class":str(pred)}, indent=4)
+    return json_results
+"""
+    from predict import predict
+    predict({"data_in": {"powerConsumption": 0.175, "load": 0.73}})
+	Sample input
+	power 	load
+	0.26  	0.13	
+	0.81	1	
+	0.15	0.07	
+
+"""
